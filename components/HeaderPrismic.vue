9647a662233aed7153cce49ba448a9ff6236c438
alexkasongo <alex.kasongolenge@yahoo.com>
2020-08-03 16:02:02 -0700

    added archive page

--- components/HeaderPrismic.vue
+++ components/HeaderPrismic.vue
@@ -9,8 +9,9 @@
     <nuxt-link to="/" class="logo">{{
       $prismic.asText($store.state.menu.title)
     }}</nuxt-link>
+    <nuxt-link to="/archive" class="logo">Archive</nuxt-link>
     <nav>
-      <ul>
+      <ul class="site-header__ul">
         <li v-for="menuLink in $store.state.menu.menu_links" :key="menuLink.id">
           <prismic-link :field="menuLink.link">{{
             $prismic.asText(menuLink.label)
@@ -27,62 +28,4 @@ export default {
 };
 </script>
 
-<style lang="sass">
-.site-header
-  height: 30px
-  padding: 20px 0
-  color: #484d52
-  font-weight: 700
-  a
-    color: #484d52
-    font-weight: 700
-  nav a:hover
-    color: #72767B
-
-.homepage .site-header
-  color: #000
-  a
-    color: #000
-  nav a:hover
-    color: #c8c9cb
-
-.site-header
-  .logo
-    display: inline-block
-    font-size: 22px
-    font-weight: 900
-  nav
-    float: right
-    ul
-      margin: 0
-      padding-left: 0
-    li
-      display: inline-block
-      margin-left: 40px
-
-@media (max-width: 1060px)
-  .site-header
-    padding-left: 20px
-    padding-right: 20px
-
-@media (max-width: 767px)
-  .site-header
-    height: auto
-
-  .homepage .site-header
-    position: absolute
-    left: 0
-    right: 0
-
-  .site-header
-    .logo
-      display: block
-      text-align: center
-    nav
-      float: none
-      text-align: center
-      li
-        display: inline-block
-        margin-left: 10px
-        margin-right: 10px
-</style>
+<style lang="scss" scoped></style>
---
+++ pages/archive.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Archive Page Works</h1>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(`archive.vue - 8 - archive page works`);
+  }
+};
+</script>
+
+<style></style>
