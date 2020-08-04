6280c28eb4f0a17ecc8b2b8d1bc470b0c3ae7900
alexkasongo <alex.kasongolenge@yahoo.com>
2020-08-03 19:42:26 -0700

    update ig link

--- components/FooterPrismic.vue
+++ components/FooterPrismic.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer">
-    <a href="https://github.com/alexkasongo" target="_blank" rel="noopener">
+    <a href="https://instagram.com/alexkasongo" target="_blank" rel="noopener">
       instagram
     </a>
     <!-- <a href="https://github.com/alexkasongo" target="_blank" rel="noopener">
@@ -10,7 +10,7 @@
         alt="Alex Kasongo"
       />
     </a> -->
-    <a href="https://instagram.com/alexkasongo" target="_blank" rel="noopener">
+    <a href="https://github.com/alexkasongo" target="_blank" rel="noopener">
       email
     </a>
   </footer>
