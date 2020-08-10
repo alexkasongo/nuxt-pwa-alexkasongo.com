6bcd3e2ae821b6bcde597c7774f0ed2c2a76dbfa
alexkasongo <alex.kasongolenge@yahoo.com>
2020-08-09 18:41:05 -0700

    reomove footer logo

--- components/FooterPrismic.vue
+++ components/FooterPrismic.vue
@@ -1,24 +1,13 @@
 <template>
   <footer class="footer">
-    <a href="https://instagram.com/alexkasongo" target="_blank" rel="noopener">
-      instagram
-    </a>
-    <!-- <a href="https://github.com/alexkasongo" target="_blank" rel="noopener">
-      <img
-        class="footer__logo"
-        src="~assets/images/aleko.png"
-        alt="Alex Kasongo"
-      />
-    </a> -->
-    <a href="https://github.com/alexkasongo" target="_blank" rel="noopener">
-      email
-    </a>
+    <a href="https://instagram.com/alexkasongo" target="_blank" rel="noopener">instagram</a>
+    <a href="https://github.com/alexkasongo" target="_blank" rel="noopener">email</a>
   </footer>
 </template>
 
 <script>
 export default {
-  name: "FooterPrismic"
+  name: "FooterPrismic",
 };
 </script>
 
